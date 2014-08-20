a470be5c05ca6166d40815896801d74d7e970f8d
Justetz <justetz@gmail.com>
2014-08-20 17:56:35 -0400

    addition of all nodes, app restructuring and maintenance

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,42 +3,51 @@
  */
 
 // =============== GOOGLE MAPS CODE ===============
-
 var map;
-function initialize() {
+
+google.maps.event.addDomListener(window, 'load', function() {
   var mapOptions = {
+    // Zoom and Location
     zoom: 17,
     center: new google.maps.LatLng(42.730174, -73.678784),
-
     // Controls
     panControl: false,
     zoomControl: true,
     mapTypeControl: true,
     scaleControl: true,
     streetViewControl: false,
-    overviewMapControl: false
+    overviewMapControl: false,
+    minZoom: 17
   };
+  // Map Style
   var styles = [
-    {
-      "featureType": "transit.station.bus",
-      "stylers": [{ "visibility": "off" }]
-    },
-    {
-      "featureType": "poi",
-      "stylers": [{ "visibility": "off" }]
-    },
-    {
-      "featureType": "landscape.man_made",
-      "elementType": "geometry.stroke",
-      "stylers": [{ "visibility": "on" }, { "color": "#000000"}]
-    }
+    {"featureType": "transit.station.bus", "stylers": [{ "visibility": "off" }]},
+    {"featureType": "poi", "stylers": [{ "visibility": "off" }]},
+    {"featureType": "landscape.man_made", "elementType": "geometry.stroke", "stylers": [{ "visibility": "on" }, { "color": "#000000"}]}
   ];
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
   map.setOptions({ styles: styles });
-}
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  var num = markers.length;
+
+  for (var i = 0; i < num; i++) {
+	var marker = new MarkerWithLabel({
+    	position: markers[i].pos,
+    	draggable: false,
+    	raiseOnDrag: false,
+    	map: map,
+    	labelContent: markers[i].label,
+    	labelAnchor: new google.maps.Point(40, 0),
+    	labelClass: "label",
+     	labelStyle: {opacity: 1.0},
+     	icon: {}
+    });
+    
+	google.maps.event.addListener(marker, "click", function (e) {
+    });
+  }
+});
 
 // ============= END GOOGLE MAPS CODE =============
 
